e5b54095c37bb9bfd340c7790ed0f9c024e0d41f
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-18 00:07:58 -0600

    Records and displays guesses.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,34 @@
 import { useState } from "react"
 import { Container } from "react-bootstrap"
-import Button from "react-bootstrap/esm/Button"
+import HexToRgb from "../utilities/HexToRGB"
 import { GuessEntry } from "./GuessEntry"
 
 export const Home = () => {
 
+    const [guesses, setGuesses] = useState<Array<any>>([])
 
+    const recordGuess = (hexGuess: string) => {
+        const RGBGuess = HexToRgb(hexGuess)
+        setGuesses([...guesses, RGBGuess])
+    }
+
+    function displayGuessed() {
+        var output = guesses.map((RGB, idx) =>
+
+            <p key={idx}>
+                <span className="px-1">{RGB.r}</span>
+                <span className="px-1">{RGB.g}</span>
+                <span className="px-1">{RGB.b}</span>
+            </p>
+
+        )
+        return (output)
+    }
 
     return (
         <Container>
-            <GuessEntry />
-
-
+            <GuessEntry recordGuess={recordGuess} />
+            {displayGuessed()}
 
         </Container>
     )
