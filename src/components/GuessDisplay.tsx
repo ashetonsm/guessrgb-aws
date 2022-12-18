471d1e09d167659284de8ec2ba670199a1fcad79
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-18 17:10:58 -0600

    Changes the span's text color if correct

--- src/components/GuessDisplay.tsx
+++ src/components/GuessDisplay.tsx
@@ -2,19 +2,25 @@ import { Card, ListGroup } from "react-bootstrap";
 
 export const GuessDisplay = ({ guesses }: { guesses: any }) => {
 
-    var output = guesses.map((RGB: { r: number; g: number; b: number; }, idx: number) =>
+    var output = guesses.map((RGB: { r: number; g: number; b: number; correct: Array<number> }, idx: number) =>
 
         <Card key={idx} className="text-center">
             <Card.Header as="h5" >Guess {idx + 1}</Card.Header>
             <ListGroup variant="flush" style={{ backgroundColor: `rgb(${RGB.r}, ${RGB.g}, ${RGB.b})` }}>
                 <ListGroup.Item style={{ backgroundColor: "inherit" }}>
-                    <span>{RGB.r}</span>
+                    <span style={{ color: RGB.correct[0] === 1 ? '#9dff00' : '#000' }}>
+                        {RGB.r}
+                    </span>
                 </ListGroup.Item>
                 <ListGroup.Item style={{ backgroundColor: "inherit" }}>
-                    <span>{RGB.g}</span>
+                    <span style={{ color: RGB.correct[1] === 1 ? '#9dff00' : '#000' }}>
+                        {RGB.g}
+                    </span>
                 </ListGroup.Item>
                 <ListGroup.Item style={{ backgroundColor: "inherit" }}>
-                    <span>{RGB.b}</span>
+                    <span style={{ color: RGB.correct[2] === 1 ? '#9dff00' : '#000' }}>
+                        {RGB.b}
+                    </span>
                 </ListGroup.Item>
             </ListGroup>
         </Card>
