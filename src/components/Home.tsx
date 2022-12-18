ed784721cd788029097205907c43aea828fe20c1
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-18 17:11:50 -0600

    Adds "correct" property to the guess

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Container } from "react-bootstrap"
 import HexToRgb from "../utilities/HexToRGB"
+import CheckGuess from "./CheckGuess"
 import GuessDisplay from "./GuessDisplay"
 import { GuessEntry } from "./GuessEntry"
 
@@ -10,14 +11,20 @@ export const Home = () => {
     const [correctAnswer, setCorrectAnswer] = useState({
         r: Math.round(Math.random() * 255),
         g: Math.round(Math.random() * 255),
-        b: Math.round(Math.random() * 255),
-    }
-    )
+        b: Math.round(Math.random() * 255)
+    })
 
     const recordGuess = (hexGuess: string) => {
         console.log(`The correct answer is: ${correctAnswer.r}, ${correctAnswer.g}, ${correctAnswer.b} `)
-        const RGBGuess = HexToRgb(hexGuess)
-        setGuesses([...guesses, RGBGuess])
+        const rgbValue = HexToRgb(hexGuess)
+        const userAnswer = {
+            r: rgbValue!.r,
+            b: rgbValue!.b,
+            g: rgbValue!.g,
+            correct: CheckGuess(correctAnswer, { r: rgbValue!.r, b: rgbValue!.b, g: rgbValue!.g })
+        }
+        console.log(userAnswer)
+        setGuesses([...guesses, userAnswer])
     }
 
     return (
