62710c35dec812de5a9c39ae8153d96f0707179d
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-18 19:21:02 -0600

    Moved flex styling out into Home, disabled button

--- src/components/GuessEntry.jsx
+++ src/components/GuessEntry.jsx
@@ -1,27 +1,26 @@
 import { useState } from "react"
 import Button from "react-bootstrap/esm/Button"
 
-export const GuessEntry = ({ recordGuess }) => {
+export const GuessEntry = ({ recordGuess, gamePlaying }) => {
 
     const [currentColor, setCurrentColor] = useState("#000000")
 
     return (
-        <div className="d-flex justify-content-center">
-            <div className="flex-column text-center">
-                <h5 className="display-5">Please choose a color:</h5>
-                <input
-                    id="colorPicker"
-                    type="color"
-                    onChange={(e) => {
-                        setCurrentColor(e.target.value)
-                    }}
-                />
-                <div className="text-center">
-                    <Button onClick={(e) => {
+        <div className="mt-2 mb-2 px-2">
+            <input
+                id="colorPicker"
+                type="color"
+                onChange={(e) => {
+                    setCurrentColor(e.target.value)
+                }}
+            />
+            <div className="text-center">
+                <Button
+                    disabled={!gamePlaying}
+                    onClick={() => {
                         console.log("Current color: " + currentColor)
                         recordGuess(currentColor)
                     }}>Submit Guess</Button>
-                </div>
             </div>
         </div>
     )
