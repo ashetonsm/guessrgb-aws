f0435e71acc9aed3bc8cc798258f6dd47a337864
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-18 19:21:28 -0600

    Added gamePlaying and gameWon.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Container } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
 import HexToRgb from "../utilities/HexToRGB"
 import CheckGuess from "./CheckGuess"
 import GuessDisplay from "./GuessDisplay"
@@ -7,6 +7,8 @@ import { GuessEntry } from "./GuessEntry"
 
 export const Home = () => {
 
+    const [gamePlaying, setGamePlaying] = useState(true)
+    const [gameWon, setGameWon] = useState(false)
     const [guesses, setGuesses] = useState<Array<any>>([])
     const [correctAnswer, setCorrectAnswer] = useState({
         r: Math.round(Math.random() * 255),
@@ -14,24 +16,62 @@ export const Home = () => {
         b: Math.round(Math.random() * 255)
     })
 
-    const recordGuess = (hexGuess: string) => {
+    const recordGuess = (hexValue: string) => {
         console.log(`The correct answer is: ${correctAnswer.r}, ${correctAnswer.g}, ${correctAnswer.b} `)
-        const rgbValue = HexToRgb(hexGuess)
+        const rgbValue = HexToRgb(hexValue)
         const userAnswer = {
             r: rgbValue!.r,
-            b: rgbValue!.b,
             g: rgbValue!.g,
-            correct: CheckGuess(correctAnswer, { r: rgbValue!.r, b: rgbValue!.b, g: rgbValue!.g })
+            b: rgbValue!.b,
+            correct: CheckGuess(correctAnswer, { r: rgbValue!.r, g: rgbValue!.g, b: rgbValue!.b })
         }
-        console.log(userAnswer)
+
+        var numCorrect = 0
+        userAnswer.correct.forEach(element => {
+            if (element === 1) { numCorrect++ }
+        })
+        if (numCorrect === 3) {
+            console.log("You win!")
+            setGameWon(true)
+            setGamePlaying(false)
+        }
+
         setGuesses([...guesses, userAnswer])
+        if (guesses.length >= 4) {
+            setGamePlaying(false)
+        }
+    }
+
+    const resetGame = () => {
+        setGuesses([])
+        setCorrectAnswer({
+            r: Math.round(Math.random() * 255),
+            g: Math.round(Math.random() * 255),
+            b: Math.round(Math.random() * 255)
+        })
+        setGameWon(false)
+        setGamePlaying(true)
     }
 
     return (
         <Container>
-            <GuessEntry recordGuess={recordGuess} />
-            <div className="d-flex gap-3">
-                <GuessDisplay guesses={guesses} />
+            <div className="d-flex justify-content-center">
+                <div className="flex-column text-center">
+                    <h5>{gamePlaying ? "Choose a color:" : gameWon ? "You win!" : "You lose!"}</h5>
+                    <GuessEntry recordGuess={recordGuess} gamePlaying={gamePlaying} />
+                    <div className="mt-2 mb-2 px-2 ">
+                        <Button
+                            style={{ visibility: gamePlaying ? 'hidden' : 'visible' }}
+                            onClick={() => {
+                                resetGame()
+                            }}>
+                            Play Again
+                        </Button>
+                    </div>
+                    <div className="d-flex gap-3">
+                        <GuessDisplay guesses={guesses} />
+                    </div>
+                </div>
             </div>
 
         </Container>
