bd3fdfa71857c70afeb5ff08bb3e1859cb704416
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-18 19:20:19 -0600

    Added animation and changed text color to white.

--- src/components/GuessDisplay.tsx
+++ src/components/GuessDisplay.tsx
@@ -4,21 +4,21 @@ export const GuessDisplay = ({ guesses }: { guesses: any }) => {
 
     var output = guesses.map((RGB: { r: number; g: number; b: number; correct: Array<number> }, idx: number) =>
 
-        <Card key={idx} className="text-center">
+        <Card key={idx} className="text-center slideDown">
             <Card.Header as="h5" >Guess {idx + 1}</Card.Header>
-            <ListGroup variant="flush" style={{ backgroundColor: `rgb(${RGB.r}, ${RGB.g}, ${RGB.b})` }}>
+            <ListGroup id="guessContainer" variant="flush" style={{ backgroundColor: `rgb(${RGB.r}, ${RGB.g}, ${RGB.b})` }}>
                 <ListGroup.Item style={{ backgroundColor: "inherit" }}>
-                    <span style={{ color: RGB.correct[0] === 1 ? '#9dff00' : '#000' }}>
+                    <span style={{ color: RGB.correct[0] === 1 ? '#9dff00' : '#FFF' }}>
                         {RGB.r}
                     </span>
                 </ListGroup.Item>
                 <ListGroup.Item style={{ backgroundColor: "inherit" }}>
-                    <span style={{ color: RGB.correct[1] === 1 ? '#9dff00' : '#000' }}>
+                    <span style={{ color: RGB.correct[1] === 1 ? '#9dff00' : '#FFF' }}>
                         {RGB.g}
                     </span>
                 </ListGroup.Item>
                 <ListGroup.Item style={{ backgroundColor: "inherit" }}>
-                    <span style={{ color: RGB.correct[2] === 1 ? '#9dff00' : '#000' }}>
+                    <span style={{ color: RGB.correct[2] === 1 ? '#9dff00' : '#FFF' }}>
                         {RGB.b}
                     </span>
                 </ListGroup.Item>
