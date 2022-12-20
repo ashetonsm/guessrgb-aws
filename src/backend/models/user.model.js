8e0d222f92fc6b8d36bd0141a316703c7dd4cab4
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-20 11:49:28 -0600

    Removed username

--- src/backend/models/user.model.js
+++ src/backend/models/user.model.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 // Information needed to create a new User
 const User = new mongoose.Schema(
     {
-        username: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
     },
