d10d05925c426a051b70706ab0f451baa5aea86d
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-27 16:20:03 -0600

    Added new Providers.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
 import { Home } from "./components/Home";
+import { GameProvider } from './context/GameContext';
+import { LoginProvider } from './context/LoginContext';
 
 function App() {
 
   return (
-    <>
+    <LoginProvider>
       <h1 className="text-center">guessRGB</h1>
-      <Home />
-    </>
+      <GameProvider>
+        <Home />
+      </GameProvider>
+    </LoginProvider>
   );
 }
 
