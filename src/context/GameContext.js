1679b3f4c2978782d6471c537ac3b52908981767
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-27 16:19:05 -0600

    Created context and reducer for gameplay.

---
+++ src/context/GameContext.js
@@ -0,0 +1,31 @@
+import { createContext, useReducer } from "react";
+import gameReducer from "./GameReducer";
+
+const GameContext = createContext();
+
+export const GameProvider = ({ children }) => {
+    const initialState = {
+        gamePlaying: true,
+        gameWon: false,
+        guesses: [],
+        correctAnswer: {
+            r: Math.round(Math.random() * 255),
+            g: Math.round(Math.random() * 255),
+            b: Math.round(Math.random() * 255)
+        }
+    };
+
+    const [state, dispatch] = useReducer(gameReducer, initialState);
+
+    return (
+        <GameContext.Provider
+            value={{
+                ...state,
+                dispatch,
+            }}>
+            {children}
+        </GameContext.Provider>
+    );
+}
+
+export default GameContext;
