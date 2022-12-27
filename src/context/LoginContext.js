6d32f720665bf5a356ba9652901e09e7ab543c03
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-27 16:18:44 -0600

    Created context and reducer for login.

---
+++ src/context/LoginContext.js
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from "react";
+import loginReducer from "./LoginReducer";
+
+const LoginContext = createContext();
+
+export const LoginProvider = ({ children }) => {
+    const initialState = {
+        isLoading: false,
+    };
+
+    const [state, dispatch] = useReducer(loginReducer, initialState);
+
+    return (
+        <LoginContext.Provider
+            value={{
+                ...state,
+                dispatch,
+            }}>
+            {children}
+        </LoginContext.Provider>
+    );
+}
+
+export default LoginContext;
---
+++ src/context/LoginReducer.js
@@ -0,0 +1,14 @@
+const loginReducer = (state, action) => {
+    switch (action.type) {
+
+        case 'SET_LOADING':
+            return {
+                ...state,
+                isLoading: action.payload,
+            };
+        default:
+            return state
+    };
+}
+
+export default loginReducer;
