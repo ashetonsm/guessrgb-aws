79a8b24b7a7b060bfb6979dd98e80dbae3ffc654
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-12-30 16:19:25 -0600

    Removed providers as they're in index now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import './App.css';
 import { Home } from "./pages/Home";
-import { GameProvider } from './context/GameContext';
-import { LoginProvider } from './context/LoginContext';
 import { Profile } from './pages/Profile';
 import { Menu } from './components/Menu';
 
 function App() {
 
   return (
-    <LoginProvider>
-      <GameProvider>
-      <h1 className="text-center">guessRGB</h1>
-      <Menu/>
-        <Profile/>
-        <Home />
-      </GameProvider>
-    </LoginProvider>
+    <>
+      <Menu />
+      <Profile />
+      <Home />
+    </>
   );
 }
 
