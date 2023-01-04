cb46cf9a325a033eb9f8ac0404367cab18ce80cb
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-01-03 19:49:16 -0600

    Tempo code to work with rough game history display

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,17 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Container } from "react-bootstrap"
+import { GuessDisplayH } from "../components/GuessDisplayH";
 import LoginContext from "../context/LoginContext";
 
 export const Profile = () => {
+    const [gameHistory, setGameHistory] = useState(null);
 
     const { dispatch, userId } = useContext(LoginContext);
 
     const fetchHistory = async () => {
         var result;
+
+        if (gameHistory == null) {
 
         if (userId !== null) {
             const response = await fetch(`http://localhost:5000/api/games/${userId}`,
@@ -21,13 +25,18 @@ export const Profile = () => {
             const data = await response.json()
             if (data.status === "success") {
                 result = data;
+                console.log(data.history)
+                setGameHistory(data.history)
 
             } else {
                 result = data;
             }
         }
+        // console.log(gameHistory)
         return console.log(result)
     }
+}
+
 
     return (fetchHistory(),
         <>
@@ -35,6 +44,7 @@ export const Profile = () => {
                 <h3>{userId ? `Hello!` : "You're not logged in!"}</h3>
 
                 <h4>This is your game history:</h4>
+                {gameHistory !== null ? <GuessDisplayH games={gameHistory}/> : null}
             </Container>
         </>
     )
