4eb07224e6bb7fc0f74ec6adff31f1070ad56663
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-01-05 14:59:35 -0600

    set userId state and cookie on login. Removed logs

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import LoginContext from "../context/LoginContext";
 
 export const Login = () => {
+
+    const { dispatch } = useContext(LoginContext);
 
     const [validated, setValidated] = useState(false);
     const [inputs, setInputs] = useState({
@@ -23,13 +26,9 @@ export const Login = () => {
         setValidated(true);
         const form = e.currentTarget.parentElement;
         if (form.checkValidity() === false) {
-            // console.log(form.checkValidity());
             return e.stopPropagation();
         }
-
-        // console.log(form.checkValidity());
-
-        await fetch(`http://localhost:5000/api/login`,
+        const request = await fetch(`http://localhost:5000/api/login`,
             {
                 method: 'POST',
                 headers: {
@@ -38,59 +37,62 @@ export const Login = () => {
                 credentials: 'include',
                 body: JSON.stringify(inputs)
             })
-            .then((response) => {
-                console.log(response);
-                if (response.status == 200) {
-                    alert("Log in successful!");
-                } else {
-                    alert("Log in unsuccessful.");
-                }
-            })
+
+        const response = await request.json()
+        console.log(response);
+        if (response.status === 'success') {
+            alert("Log in successful!");
+            document.cookie = `userId=${response.session.userId}; expires=${new Date(response.session.cookie.expires).toUTCString()}; path=${response.session.cookie.path}`;
+            dispatch({ type: 'SET_USERID', payload: response.session.userId });
+        } else {
+            alert("Log in unsuccessful.");
+        }
     }
 
-    return (
-        <Form noValidate validated={validated}>
-            <Form.Group className="mb-3">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    required
-                    type="email"
-                    id="email"
-                    minLength={6}
-                    maxLength={50}
-                    value={inputs.email}
-                    onChange={handleChange} />
-                <Form.Text>Please enter your email address.</Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    required
-                    type="password"
-                    id="password"
-                    minLength={8}
-                    maxLength={12}
-                    value={inputs.password}
-                    onChange={handleChange} />
-                <Form.Text>Please enter your password.</Form.Text>
-            </Form.Group>
 
-            <Form.Group className="mb-3 text-start">
-                <Form.Check
-                    type="checkbox"
-                    id="rememberUser"
-                    label="Remember me"
-                    value={inputs.rememberUser}
-                    onChange={(e) => {
-                        const { id, value } = e.target
-                        setInputs((inputs) => ({
-                            ...inputs,
-                            [id]: value === "false" ? "true" : "false",
-                        }))
-                    }}
-                />
-            </Form.Group>
-            <Button type="submit" onClick={handleSubmit}>Log in</Button>
-        </Form>
-    )
+return (
+    <Form noValidate validated={validated}>
+        <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+                required
+                type="email"
+                id="email"
+                minLength={6}
+                maxLength={50}
+                value={inputs.email}
+                onChange={handleChange} />
+            <Form.Text>Please enter your email address.</Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                required
+                type="password"
+                id="password"
+                minLength={8}
+                maxLength={12}
+                value={inputs.password}
+                onChange={handleChange} />
+            <Form.Text>Please enter your password.</Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3 text-start">
+            <Form.Check
+                type="checkbox"
+                id="rememberUser"
+                label="Remember me"
+                value={inputs.rememberUser}
+                onChange={(e) => {
+                    const { id, value } = e.target
+                    setInputs((inputs) => ({
+                        ...inputs,
+                        [id]: value === "false" ? "true" : "false",
+                    }))
+                }}
+            />
+        </Form.Group>
+        <Button type="submit" onClick={handleSubmit}>Log in</Button>
+    </Form>
+)
 }
