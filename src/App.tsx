5746cc43a3a3cf6deb6fe9a4da90e5434c5596c8
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-01-05 20:05:24 -0600

    Rearranged routing, added profile redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import './App.css';
-import { Home } from "./pages/Home";
-import { Profile } from './pages/Profile';
 import { Menu } from './components/Menu';
+import { useContext } from 'react';
+import LoginContext from './context/LoginContext';
+import { useLocation, Navigate, Route, Routes } from 'react-router-dom';
+import { Profile } from './pages/Profile';
+import { Home } from './pages/Home';
 
-function App() {
+export default function App() {
+
+  const { userId } = useContext(LoginContext);
+
+  function RequireAuth({ children }: { children: JSX.Element }) {
+    let location = useLocation();
+
+    if (!userId) {
+      // Redirect to home
+      return <Navigate to="/guessRGB/" />;
+    }
+
+    return children;
+  }
 
   return (
-    <>
-      <Menu />
-      <Profile />
-      <Home />
-    </>
+    <Routes>
+      <Route element={<Menu loggedIn={userId ? true : false}/>}>
+        <Route path="guessRGB/" element={<Home />} />
+        <Route path="guessRGB/profile"
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          } />
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
