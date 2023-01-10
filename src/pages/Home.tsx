2de3508d52ddc35bd4478279881e61b111d1bb90
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-01-10 15:10:37 -0600

    Records difficulty in result.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,8 @@ export const Home = () => {
                 status: gameWon ? 1 : 0,
                 date: new Date().toUTCString(),
                 guesses: guesses,
-                answer: correctAnswer
+                answer: correctAnswer,
+                difficulty: difficulty
             }
         }
 
@@ -93,29 +94,26 @@ export const Home = () => {
     }
 
     return (
-        <>
-            <Container>
-                {!gamePlaying && <AnswerToast correctAnswer={correctAnswer} />}
-                <div className="d-flex justify-content-center">
-                    <div className="flex-column text-center">
-                        <h5>{gamePlaying ? "Choose a color:" : gameWon ? "You win!" : "You lose!"}</h5>
-                        <GuessEntry recordGuess={recordGuess} gamePlaying={gamePlaying} />
-                        <div className="mt-2 mb-2 px-2 ">
-                            <Button
-                                style={{ visibility: gamePlaying ? 'hidden' : 'visible' }}
-                                onClick={() => {
-                                    resetGame()
-                                }}>
-                                Play Again
-                            </Button>
-                        </div>
-                        <div className="d-flex gap-3">
-                            <GuessDisplay guesses={guesses} />
-                        </div>
+        <Container>
+            {!gamePlaying && <AnswerToast correctAnswer={correctAnswer} />}
+            <div className="d-flex justify-content-center">
+                <div className="flex-column text-center">
+                    <h5>{gamePlaying ? "Choose a color:" : gameWon ? "You win!" : "You lose!"}</h5>
+                    <GuessEntry recordGuess={recordGuess} gamePlaying={gamePlaying} />
+                    <div className="mt-2 mb-2 px-2 ">
+                        <Button
+                            style={{ visibility: gamePlaying ? 'hidden' : 'visible' }}
+                            onClick={() => {
+                                resetGame()
+                            }}>
+                            Play Again
+                        </Button>
+                    </div>
+                    <div className="d-flex gap-3">
+                        <GuessDisplay guesses={guesses} />
                     </div>
                 </div>
-
-            </Container>
-        </>
+            </div>
+        </Container>
     )
 }
