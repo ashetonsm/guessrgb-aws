65742986b0045b8d7f7b26727d9a6d6a9d420c84
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-01-10 18:21:32 -0600

    Adjusted to go with Home changes. New ID.

--- src/components/GuessDisplay.tsx
+++ src/components/GuessDisplay.tsx
@@ -4,9 +4,16 @@ export const GuessDisplay = ({ guesses }: { guesses: any }) => {
 
     var output = guesses.map((RGB: { r: number; g: number; b: number; correct: Array<number> }, idx: number) =>
 
-        <Card key={idx} className="text-center slideDown">
+        <Card
+            key={idx}
+            className="text-center slideDown d-flex flex-wrap"
+            style={{ minWidth: '5.5em', maxWidth: '1em' }}
+        >
             <Card.Header as="h5" >Guess {idx + 1}</Card.Header>
-            <ListGroup id="guessContainer" variant="flush" style={{ backgroundColor: `rgb(${RGB.r}, ${RGB.g}, ${RGB.b})` }}>
+            <ListGroup
+                id={`guessContainer-${idx}`}
+                variant="flush"
+                style={{ backgroundColor: `rgb(${RGB.r}, ${RGB.g}, ${RGB.b})` }}>
                 <ListGroup.Item style={{ backgroundColor: "inherit" }}>
                     <span style={{ color: RGB.correct[0] === 1 ? '#9dff00' : '#FFF' }}>
                         {RGB.r}
