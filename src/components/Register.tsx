e3f8e76cf66dbbfe5a52e0e4c1d7e9fc5fce0aeb
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-01-23 11:36:14 -0600

    Added recaptcha to registration page.

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { ReCaptcha } from "./ReCaptcha";
 
 export const Register = () => {
 
     const [validated, setValidated] = useState(false);
+    const [recaptchaWarning, setRecaptchaWarning] = useState(false);
     const [inputs, setInputs] = useState({
         email: "",
-        password: ""
+        password: "",
+        token: ''
     });
 
     const handleChange = (e: { target: { id: string; value: string; }; }) => {
@@ -19,29 +22,62 @@ export const Register = () => {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
+        console.log(inputs)
         setValidated(true);
         const form = e.currentTarget.parentElement;
-        if (form.checkValidity() === false) {
-            console.log(form.checkValidity());
+        if (form.checkValidity() === false ||
+            inputs.token === null ||
+            inputs.token === '') {
+            if (inputs.token === null || inputs.token === '') {
+                setRecaptchaWarning(true);
+            } else {
+                setRecaptchaWarning(false);
+            }
             return e.stopPropagation();
         }
+        var verifiedToken = false;
 
-        const response = await fetch(`http://localhost:5000/api/register`,
+        verifiedToken = await verifyToken()
+        console.log(verifiedToken)
+
+        if (verifiedToken) {
+            const response = await fetch(`http://localhost:5000/api/register`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(inputs)
+                }
+            )
+            const data = await response.json()
+            if (data.status === "success") {
+                console.log(data)
+                alert("Registration successful!");
+            } else {
+                console.log(data)
+                alert("Registration unsuccessful.");
+            }
+        }
+    }
+
+    const verifyToken = async () => {
+        const request = await fetch(`http://localhost:5000/api/verify`,
             {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(inputs)
-            }
-        )
-        const data = await response.json()
-        if (data.status === "success") {
-            console.log(data)
-            alert("Registration successful!");
+                credentials: 'include',
+                body: JSON.stringify({ token: inputs.token })
+            })
+
+        const response = await request.json()
+        console.log(response);
+        if (response.status === 'success') {
+            return true
         } else {
-            console.log(data)
-            alert("Registration unsuccessful.");
+            return false
         }
     }
 
@@ -72,6 +108,17 @@ export const Register = () => {
                     onChange={handleChange} />
                 <Form.Text>Please enter a password (8-12 characters).</Form.Text>
             </Form.Group>
+            <div id="reCaptcha-box"
+                className="mb-3"
+                style={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    backgroundColor: `${recaptchaWarning ? '#dc3545' : validated ? '#198754' : 'transparent'}`,
+                    borderRadius: 5,
+                    width: 'fit-content'
+                }}>
+                <ReCaptcha setInputs={setInputs} />
+            </div>
             <Button type="submit" onClick={handleSubmit}>Register</Button>
         </Form>
     )
