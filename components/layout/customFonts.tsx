fe5b19d284da2db45fd226cb916d448439f1c299
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-02-05 17:50:55 -0600

    Style and ignore updates

--- components/layout/customFonts.tsx
+++ components/layout/customFonts.tsx
@@ -1,7 +1,7 @@
 export default function CustomFonts() {
     return (
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Modak&display=swap');
+            {/* @import url('https://fonts.googleapis.com/css2?family=Modak&display=swap'); */}
         </style>
     );
 }
