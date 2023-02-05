3db8714800be57c3576e06c2951381a94fa7b6cc
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-02-05 13:35:48 -0600

    updated case

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import LoadingDots from '@/components/icons/loading-dots';
 import { ReactNode } from 'react';
 import CustomFonts from './customFonts';
-import { Menu } from './Menu';
+import { Menu } from './menu';
 
 export default function Layout({ meta, children }: { meta: MetaProps; children: ReactNode }) {
   const router = useRouter();
