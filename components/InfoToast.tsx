f7ffab399cee2214b94076b4376fc868600bfaf3
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-02-07 13:19:50 -0600

    Fixed onHide error message

--- components/InfoToast.tsx
+++ components/InfoToast.tsx
@@ -1,24 +1,26 @@
 import Toast from 'react-bootstrap/Toast';
 import ToastContainer from 'react-bootstrap/ToastContainer';
-import { ToastContainerProps } from "react-bootstrap"
 import { SetStateAction } from 'react';
 
-export const InfoToast = (
-    props: JSX.IntrinsicAttributes & ToastContainerProps &
-    { msg: string; show: string; onHide: (value: SetStateAction<boolean>) => void; }) => {
+interface Props {
+    msg: string
+    show: string
+    onHide: (value: SetStateAction<boolean>) => void
+}
+
+export const InfoToast: React.FC<Props> = ({ msg, show, onHide }) => {
 
     return (
         <ToastContainer
-            {...props}
             position='middle-center'
             className='mb-2 mt-2 px-2 text-center'>
             <Toast
-                show={props.show === "true" ? true : false}
-                onClose={() => props.onHide(false)}
+                show={show === "true" ? true : false}
+                onClose={() => onHide(false)}
                 autohide={true}
                 delay={3000}
             >
-                <Toast.Body style={{ color: '#000' }}>{props.msg}</Toast.Body>
+                <Toast.Body style={{ color: '#000' }}>{msg}</Toast.Body>
             </Toast>
         </ToastContainer>
     )
