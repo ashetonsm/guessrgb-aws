b87ead4f8b2208a24e1d9298f15f86deb570d3cb
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-02-09 19:44:54 -0600

    Profile gets and displays history of current user

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,62 @@
+import { GuessDisplayH } from "@/components/GuessDisplayH";
+import Paginate from "@/lib/paginate";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react"
+import { useState } from "react";
+import { Container, Button } from "react-bootstrap";
 
 const Profile = ({ history, currentUser }: { history?: any, currentUser: string }) => {
-    console.log(history.history)
-    return (`Welcome, ${currentUser}!`)
+    const [pageNumber, setPageNumber] = useState(1)
+
+    console.log(history)
+
+    return (
+        <Container>
+            <div className="text-center d-flex flex-wrap justify-content-center"
+            >
+                <h3>Hello {currentUser}! This is your game history:</h3>
+            </div>
+            {/* 
+        <div className="d-flex flex-wrap justify-content-center gap-3">
+            <Button
+                onClick={() => fetchHistory()}
+                className="btn-light btn-outline-light mb-2">🔄</Button>
+        </div> 
+        */}
+
+            {history ?
+                <div>
+                    <div className="d-flex flex-wrap justify-content-center gap-3 mb-2">
+                        <Button
+                            disabled={pageNumber - 1 > 0 ? false : true}
+                            onClick={() => {
+                                if ((pageNumber - 1) > 0) {
+                                    setPageNumber(pageNumber - 1)
+                                }
+                            }}>Prev. Page</Button>
+
+                        <span>Page {pageNumber}</span>
+
+                        <Button
+                            disabled={pageNumber + 1 <= history.length ? false : true}
+                            onClick={() => {
+                                if (pageNumber + 1 <= history.length) {
+                                    setPageNumber(pageNumber + 1)
+                                }
+                            }}>Next Page</Button>
+
+                    </div>
+                    <GuessDisplayH games={Paginate(history, 1, pageNumber)} />
+                </div>
+                :
+                <div className="d-flex flex-wrap justify-content-center gap-3 mb-2">
+                    <h4>
+                        No games found!
+                    </h4>
+                </div>
+            }
+        </Container>
+    )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
@@ -24,7 +77,11 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
         }
     })
 
-    const history = await getHistory.json()
+    const historyObj = await getHistory.json()
+    var history = null
+    if(historyObj.history) {
+        history = historyObj.history.reverse()
+    }
     const currentUser = session.user?.email?.toString()
 
     return {
