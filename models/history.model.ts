6d9474bae0eeb663545898b1358556548a39261c
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-02-10 00:52:23 -0600

    Removed matchingUser; will use emails now

--- models/history.model.ts
+++ models/history.model.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 // Information needed to create a new game History for a User
 const historySchema = new mongoose.Schema(
     {
-        userId: { type: String, required: true },
+        email: { type: String, required: true },
         history: { type: Array, required: true },
     },
     {collection: 'games'}
--- pages/api/[games].ts
+++ pages/api/[games].ts
@@ -13,37 +13,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     const session = await getSession({ req })
     const client = await clientPromise;
     const games = client.db('test').collection('games');
-    const users = client.db('test').collection('users');
 
     // Obj that will be used to send a response message or error
     var responseData;
 
     try {
         console.log(session ? session : "No session found")
-        const matchingUser = await users.findOne({
-            email: { $regex: `${session?.user?.email}`, $options: 'i' },
+        await games.findOne({
+            email: session!.user!.email!.toString()
         })
-        if (matchingUser !) {
-            console.log(`Found user: ${matchingUser?._id}`)
-            await games.findOne({
-                userId: matchingUser!._id.toString()
+            .then((result) => {
+                if (result !== null) {
+                    console.log(result.history)
+                    responseData = { message: 'Search game success.', history: result.history }
+                    res.json(responseData as ResponseData)
+                } else {
+                    responseData = { message: 'Search game success. There were no games found.' }
+                    res.json(responseData as ResponseData)
+                }
             })
-                .then((result) => {
-                    if (result !== null) {
-                        console.log(result.history)
-                        responseData = { message: 'Search game success.', history: result.history }
-                        res.json(responseData as ResponseData)
-                    } else {
-                        responseData = { message: 'Search game success. There were no games found.' }
-                        res.json(responseData as ResponseData)
-                    }
-                })
-
-        } else {
-            console.log("User not found.")
-            responseData = { error: 'User not found. Search aborted.' }
-            res.json(responseData as ResponseData)
-        }
     } catch (error) {
         console.error(error)
         responseData = { error: 'Search game failure.' }
