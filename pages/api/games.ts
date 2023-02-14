de44994f549d29f054b939f218439dfb1a3f0c45
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-02-14 16:53:47 -0600

    Changed both to getServerSession, msg to message

--- pages/api/games.ts
+++ pages/api/games.ts
@@ -1,16 +1,17 @@
 import clientPromise from "@/lib/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 interface ResponseData {
     error?: string
-    msg?: string
+    message?: string
     history?: any
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
 
-    const session = await getSession({ req })
+    const session = await getServerSession(req, res, authOptions)
     const client = await clientPromise;
     const games = client.db('test').collection('games');
 
@@ -18,11 +19,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     var responseData;
 
     try {
+        
+        console.log(session)
+        if (!session) {
+            responseData = { message: 'You must be logged in.' }
+            res.status(401).json(responseData as ResponseData)
+            return;
+        }
+
         await games.findOne({
             email: session!.user!.email!.toString()
         })
             .then((result) => {
                 if (result !== null) {
+                    console.log(session)
                     console.log(`Completed games: ${result.history.length}`)
                     responseData = { message: 'Search game success.', history: result.history }
                     res.json(responseData as ResponseData)
--- pages/api/record.ts
+++ pages/api/record.ts
@@ -1,16 +1,17 @@
 import clientPromise from "@/lib/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import History from "@/models/history.model";
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 interface ResponseData {
     error?: string
-    msg?: string
+    message?: string
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
 
-    const session = await getSession({ req })
+    const session = await getServerSession(req, res, authOptions)
     const client = await clientPromise;
     const games = client.db('test').collection('games');
 
@@ -18,6 +19,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     var responseData;
 
     try {
+
+        if (!session) {
+            responseData = { message: 'You must be logged in.' }
+            res.status(401).json(responseData as ResponseData)
+            return;
+        }
 
         const newHistory = JSON.parse(req.body)
 
