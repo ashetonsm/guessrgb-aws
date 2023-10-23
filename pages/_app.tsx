993f589fd872f4db98ebcb0abd6cfeaae7f877ca
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-10-23 15:08:35 -0500

    Checkes for CognitoUser on load, passes setUser

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,38 @@
 import '@/styles/globals.css';
 import Layout from '@/components/layout';
 import '@/styles/custom.scss'
-import { GameProvider } from '@/context/GameContext';
-import { Amplify } from 'aws-amplify';
+import GameContext, { GameProvider } from '@/context/GameContext';
+import { Amplify, Auth } from 'aws-amplify';
 import awsmobile from '@/src/aws-exports';
+import { useContext, useEffect, useState } from 'react';
 
-Amplify.configure({ ...awsmobile, ssr: true})
+Amplify.configure({ ...awsmobile, ssr: true })
 
-export default function MyApp({ Component, pageProps}: any) {
+export default function MyApp({ Component, pageProps }: any) {
+  const [user, setUser] = useState<any>(null);
+  const { dispatch, isAuthenticated } = useContext(GameContext);
+
+  useEffect(() => {
+    const checkAuthenticatedUser = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser()
+        setUser(user)
+        dispatch({ type: 'SET_IS_AUTHENTICATED', payload: true })
+        console.log(user)
+      } catch (err) {
+        console.log("No cognito user is logged in")
+      }
+    }
+    if (!isAuthenticated) {
+      checkAuthenticatedUser();
+    }
+  }, [])
+
   return (
-      <GameProvider>
-        <Layout {...pageProps}>
-          <Component {...pageProps} />
-        </Layout>
-      </GameProvider>
+    <GameProvider>
+      <Layout {...pageProps}>
+        <Component {...pageProps} user={user} />
+      </Layout>
+    </GameProvider>
   );
 }
