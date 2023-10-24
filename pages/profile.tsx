04b91da9e933316efc7e7fddea3fc9de447bd7f1
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-10-23 21:48:45 -0500

    Use withSSRContext to check current user

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,7 @@
 import { GuessDisplayH } from "@/components/guessDisplayH";
 import GameContext from "@/context/GameContext";
 import Paginate from "@/lib/paginate";
+import { withSSRContext } from "aws-amplify";
 import { GetServerSideProps } from "next";
 import { useContext, useEffect, useState } from "react";
 import { Container, Button } from "react-bootstrap";
@@ -31,51 +32,59 @@ const Profile = ({ history, user }: { history?: any, user: any }) => {
 
     }, [darkMode])
 
+    useEffect(() => {
+        console.log(user)
+    }, [user])
+
     return (
-            <Container>
-                <div className="text-center d-flex flex-wrap justify-content-center">
-                    <h3>Hello {user.attributes.email}! This is your game history:</h3>
-                </div>
+        <Container>
+            <div className="text-center d-flex flex-wrap justify-content-center">
+                <h3>Hello {user ? user.attributes.email : '...'}! This is your game history:</h3>
+            </div>
 
-                {history ?
-                    <div>
-                        <div className="d-flex flex-wrap justify-content-center gap-3 mb-2">
-                            <Button
-                                disabled={pageNumber - 1 > 0 ? false : true}
-                                onClick={() => {
-                                    if ((pageNumber - 1) > 0) {
-                                        setPageNumber(pageNumber - 1)
-                                    }
-                                }}>Prev. Page</Button>
-
-                            <span>Page {pageNumber}/{history.length}</span>
-
-                            <Button
-                                disabled={pageNumber + 1 <= history.length ? false : true}
-                                onClick={() => {
-                                    if (pageNumber + 1 <= history.length) {
-                                        setPageNumber(pageNumber + 1)
-                                    }
-                                }}>Next Page</Button>
-
-                        </div>
-                        <GuessDisplayH games={Paginate(history, 1, pageNumber)} />
-                    </div>
-                    :
+            {history ?
+                <div>
                     <div className="d-flex flex-wrap justify-content-center gap-3 mb-2">
-                        <h4>
-                            No games found!
-                        </h4>
+                        <Button
+                            disabled={pageNumber - 1 > 0 ? false : true}
+                            onClick={() => {
+                                if ((pageNumber - 1) > 0) {
+                                    setPageNumber(pageNumber - 1)
+                                }
+                            }}>Prev. Page</Button>
+
+                        <span>Page {pageNumber}/{history.length}</span>
+
+                        <Button
+                            disabled={pageNumber + 1 <= history.length ? false : true}
+                            onClick={() => {
+                                if (pageNumber + 1 <= history.length) {
+                                    setPageNumber(pageNumber + 1)
+                                }
+                            }}>Next Page</Button>
+
                     </div>
-                }
-            </Container>
+                    <GuessDisplayH games={Paginate(history, 1, pageNumber)} />
+                </div>
+                :
+                <div className="d-flex flex-wrap justify-content-center gap-3 mb-2">
+                    <h4>
+                        No games found!
+                    </h4>
+                </div>
+            }
+        </Container>
     )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-    /*
-    const session = false;
-    if (!session) {
+    const { Auth } = withSSRContext({req});
+    var user = null;
+    try {
+        user = await Auth.currentAuthenticatedUser()
+        console.log(`This email address is logged in: ${user.attributes.email}`)
+    } catch (err) {
+        console.log("No cognito user is logged in")
         return {
             redirect: {
                 permanent: false,
@@ -84,6 +93,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
         };
     }
 
+    /*
     const getHistory = await fetch(`${process.env.NEXTAUTH_URL}/api/games`, {
         method: 'GET',
         headers: {
@@ -100,8 +110,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     */
     return {
         props: {
-            // history,
-            // session
+            user: JSON.parse(JSON.stringify(user))
         }
     };
 };
